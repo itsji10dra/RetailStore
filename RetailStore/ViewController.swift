f4fb3f87ee5aba1834dcf6d1bb7f9055d5dcbfb6
Jitendra <jitendra@cellpointmobile.com>
2018-10-08 16:00:26 +0530

    Initial Commit...

---
+++ RetailStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetailStore
+//
+//  Created by Jitendra on 08/10/18.
+//  Copyright © 2018 Jitendra Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
